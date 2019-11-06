0412659463869d3b441ac95569c6ea174743ed05
boniface14 <bonface14@gmail.com>
2019-11-06 09:10:41 +0300

    Routing

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ app.listen(3000,function(){
     console.log('listening on 3000')
 })
 //app.get(path, callback)
-app.get('/',function (req,res){
+app.get('/', (req,res)=>{
 res.send('Hello world')
 })
+//app.get(path, callback)
+app.get('/about', (req,res)=>{
+    res.send('about us page')
+    })
+    
