b1cd1d51aa422c4fc19d2af0285803befe656c53
boniface14 <bonface14@gmail.com>
2019-11-08 10:25:08 +0300

    pug continued with a registration form

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 //console.log('Hello world')
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+const path = require('path')
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 app.listen(3000, function () {
     console.log('listening on 3000')
 })
@@ -29,6 +34,22 @@ app.get('/users/:name', (req, res) => {
     res.send('hello  ' + req.params.name)
 })
 //error page
-app.get('*', (req, res) => {
-    res.send('Dont worry about this we will sort it soon')
+//app.get('*', (req, res) => {
+// res.send('Dont worry about this we will sort it soon')
+//})
+app.get('/users', (req, res) => {
+    res.send('This is a class' + req.query.class + 'cohort' + req.query.cohort)
+})
+app.get('/register_form', (req, res) => {
+    //    console.log('body',req.body)
+    //    console.log('query',req.query)
+    res.render('register_form')
+})
+// another method for post
+app.post('/register_form', (req, res) => {
+    console.log("form has been posted")
+    console.log('body', req.body)
+    console.log('query', req.query)
+    console.log('query params', req.query)
+
 })
